bbccb895ec0419ba591779893bea633c231bd9c2
Sean Washington <hello@seanwash.com>
2015-05-27 23:10:17 -0700

    Setup psExporter to export all icons and not a debug subset

--- jsx/psExporter.jsx
+++ jsx/psExporter.jsx
@@ -79,14 +79,14 @@
 		// parse layerSet children
 
 		// For Debugging - Set number of output icons to 25
-		for (var j = 0; j < 1; j++) {
-			exportIconLayer(layerSet.artLayers[j]);
-		}
-
-		// The real deal - export ALL of the icons ~40min
-		// for (var j = 0; j < artLayersLength; j++) {
+		// for (var j = 0; j < 1; j++) {
 		// 	exportIconLayer(layerSet.artLayers[j]);
 		// }
+
+		// The real deal - export ALL of the icons ~40min
+		for (var j = 0; j < artLayersLength; j++) {
+			exportIconLayer(layerSet.artLayers[j]);
+		}
 	}
 
 	alert('psExporter finished.');
